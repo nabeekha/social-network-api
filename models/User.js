f25a4b8be202ade3c51955edbd57cc89cd4da27f
Nabeel Khan <nabeekha@umich.edu>
2023-05-29 08:18:08 -0400

    commit with additional routes

--- models/User.js
+++ models/User.js
@@ -4,19 +4,39 @@ const userSchema = new Schema(
   {
     username: {
       type: String,
-    //   not sure if these are the correct versions 
       unique: true,
       required: true,
-      trimmed: true,
+      trim: true,
     },
-    
+    email: {
+        type: String,
+      unique: true,
+      required: true,
+      match: [/.+@.+\..+/, 'Must match an email address!'],
+    },
+    thoughts: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+    },
+    ],
+    friends: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+    },
+    ],
   },
   {
     toJSON: {
-      getters: true,
+      virtuals: true,
     },
     id: false,
   }
 );
 
-module.exports = userSchema;
+userSchema.virtual('friendCount').get(function(){
+    return this.friends.length
+})
+
+const User = model('User', userSchema)
+
+module.exports = User;
--- server.js
+++ server.js
@@ -7,11 +7,6 @@ const cwd = process.cwd();
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// // Not sure what this does or if it is necessary
-// const activity = cwd.includes('01-Activities')
-//   ? cwd.split('/01-Activities/')[1]
-//   : cwd;
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
