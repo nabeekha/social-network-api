0cc96ca7e5b891d5bf11383fc2fc98d42f22557c
Nabeel Khan <nabeekha@umich.edu>
2023-06-09 09:25:02 -0400

    commit with updated user controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -2,10 +2,21 @@ const { User, Thought } = require('../models');
 
 const userController = {
 async getAllUsers (req, res) {
-
+    User.find({})
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
 },
 async getOneUser (req, res) {
-
+    User.findOne({ _id: req.params.userId })
+      .populate("thoughts")
+      .populate("friends")
+      .select("-__v")
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No User find with that ID!" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
 },
 async createUser (req, res) {
     try {
