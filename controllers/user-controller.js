92e1c1ea1187a86815f696eac1da09f9eae52028
Nabeel Khan <nabeekha@umich.edu>
2023-06-09 09:31:41 -0400

    commit with full user controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,11 +1,14 @@
 const { User, Thought } = require('../models');
 
+
 const userController = {
+    // function to get all users
 async getAllUsers (req, res) {
     User.find({})
       .then((user) => res.json(user))
       .catch((err) => res.status(500).json(err));
 },
+// function to get one user 
 async getOneUser (req, res) {
     User.findOne({ _id: req.params.userId })
       .populate("thoughts")
@@ -18,6 +21,7 @@ async getOneUser (req, res) {
       )
       .catch((err) => res.status(500).json(err));
 },
+// function to create users
 async createUser (req, res) {
     try {
         const dbUser = await User.create(req.body)
@@ -26,6 +30,35 @@ async createUser (req, res) {
         console.log(error)
         res.status(500).json(error)
     }
+},
+//function to add a friend
+async addOneFriend (req, res) {
+    User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $addToSet: { friends: req.params.friendId } },
+        { runValidators: true, new: true }
+      )
+        .then((user) =>
+          !user
+            ? res.status(404).json({ message: "There is no user with this ID" })
+            : res.json(user)
+        )
+        .catch((err) => res.status(500).json(err));
+},
+//function to delete a friend
+async deleteOneFriend (req, res) {
+    User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $pull: { friends: req.params.friendId } },
+        { new: true }
+      )
+        .then(
+          (user) =>
+            !user
+              ? res.status(404).json({ message: "There is no user with this ID" })
+              : res.json(user)
+        )
+        .catch((err) => res.status(500).json(err));
 },
 }
 
