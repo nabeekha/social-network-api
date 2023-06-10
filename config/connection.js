275cd9d48755f14bdf151ec1b96f9c7f3979bc51
Nabeel Khan <nabeekha@umich.edu>
2023-06-10 18:29:01 -0400

    commit with test routes

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,18 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialnetworkdb');
+const connectionString = process.env.MONGODB_URI || 'mongodb://localhost:27017/socialnetworkdb'
 
-connect(connectionString, {
+mongoose.connect(connectionString, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
+
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
 
 module.exports = mongoose.connection;
