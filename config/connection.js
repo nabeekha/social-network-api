65d18a08d4253e37e0beefdf7926063dab95d699
Nabeel Khan <nabeekha@umich.edu>
2023-06-10 17:50:29 -0400

    commit with updated models, still has some bugs in thought model

--- config/connection.js
+++ config/connection.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialnetworkdb');
 
-// connect(connectionString, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   });
+connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
 
 module.exports = mongoose.connection;
