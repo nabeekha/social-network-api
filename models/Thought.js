015ca26cf343f97aed2dcd70f978ccb791e4734a
Nabeel Khan <nabeekha@umich.edu>
2023-06-10 18:01:00 -0400

    updated thought model

--- models/Thought.js
+++ models/Thought.js
@@ -66,5 +66,5 @@ thoughtSchema.virtual('reactionCount').get(function(){
     return this.reactions.length
 })
 
-const User = model('Thought', thoughtSchema)
+const Thought = model('Thought', thoughtSchema)
 module.exports = Thought;
